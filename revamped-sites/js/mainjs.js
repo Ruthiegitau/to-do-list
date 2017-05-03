12d2a9f317b0aad892ba9418f261ead847a25b3b
Pato <ruthienjerigitau@gmail.com>
2017-05-03 15:07:23 +0300

    added images and styles section1

---
+++ revamped-sites/js/mainjs.js
@@ -0,0 +1,29 @@
+$(document).ready(function functionName() {
+  $(".hover-txt-1").hide();
+  $(".hover-holder-1").mouseover(function functionName() {
+    $(".hover-txt-1").show(500);
+    $(".hover-view-1").hide(300).delay(300).fadeOut(200);
+  });
+  $(".hover-holder-1").mouseleave(function functionName() {
+    $(".hover-txt-1").hide(300);
+    $(".hover-view-1").show(500);
+  });
+  $(".hover-txt-2").hide();
+  $(".hover-holder-2").mouseover(function functionName() {
+    $(".hover-txt-2").show(500);
+    $(".hover-view-2").hide(300).delay(300).fadeOut(200);
+  });
+  $(".hover-holder-2").mouseleave(function functionName() {
+    $(".hover-txt-2").hide(300);
+    $(".hover-view-2").show(500);
+  });
+  $(".hover-txt-3").hide();
+  $(".hover-holder-3").mouseover(function functionName() {
+    $(".hover-txt-3").show(500);
+    $(".hover-view-3").hide(300).delay(300).fadeOut(200);
+  });
+  $(".hover-holder-3").mouseleave(function functionName() {
+    $(".hover-txt-3").hide(300);
+    $(".hover-view-3").show(500);
+  });
+});
