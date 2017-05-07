e019bc988b3d1cd13a66c523dfe57a9a4c78feb3
Pato <ruthienjerigitau@gmail.com>
2017-05-07 12:44:12 +0300

    informative commit message

---
+++ moringa/java/to-do-list/src/test/java/TaskTest.java
@@ -0,0 +1,26 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import java.time.LocalDateTime;
+
+public class TaskTest {
+
+  @Test
+ public void Task_instantiatesWithDescription_String() {
+   Task myTask = new Task("Mow the lawn");
+   assertEquals("Mow the lawn", myTask.getDescription());
+
+ }
+
+ @Test
+ public void isCompleted_isFalseAfterInstantiation_false(){
+   Task myTask = new Task("Mow the lawn");
+   assertEquals(false, myTask.isCompleted());
+ }
+
+ @Test
+ public void getCreatedAt_instansiatesWithCurrentTime_today(){
+   Task myTask = new Task("Mow the lawn");
+   assertEquals(LocalDateTime.now().getDayOfWeek (), myTask.getCreatedAt().getDayOfWeek)
+ }
+
+}
